3707d49ae5c8bd16f246ed9a6a56b00c45ddfa70
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-09 17:24:00 +0200

    "solution"

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -8,9 +7,9 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def movie_list(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -21,13 +20,14 @@ def movie_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
+
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
-    if request.method == 'GET':
+    if request.method == "GET":
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'PUT':
+    elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
